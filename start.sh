f4986a399701045356501354886c635c56caa584
Kevin Merfeld <kevin.merfeld@zoro.com>
2020-04-13 09:42:04 -0500

    using docker compose

--- start.sh
+++ start.sh
@@ -2,16 +2,4 @@
 
 set -euo pipefail
 
-function main(){
-
-  echo "Stopping and removing old container if exists..."
-  docker stop recipe_container || true && docker rm recipe_container || true
-
-  echo "Building image..."
-  docker build -t recipe_image .
-
-  echo "Running container..."
-  docker run --env EDAMAM_APP_ID --env EDAMAM_APP_KEY -p 5000:5000 --name recipe_container recipe_image
-}
-
-main
+docker-compose up --build --remove-orphans
